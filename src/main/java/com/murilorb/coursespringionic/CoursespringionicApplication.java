cd5da2be14f32fe8b2f37c2ce1e4fe72a15b5466
mucarb <murilo.rb60@gmail.com>
2022-07-25 16:53:36 -0300

    Project started

---
+++ src/main/java/com/murilorb/coursespringionic/CoursespringionicApplication.java
@@ -0,0 +1,13 @@
+package com.murilorb.coursespringionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursespringionicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursespringionicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murilorb/coursespringionic/CoursespringionicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilorb.coursespringionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursespringionicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
