8fd132dc8eb51c9d526c7ecddb55790eea4cbdcb
mucarb <murilo.rb60@gmail.com>
2022-07-25 21:44:49 -0300

    Testing the first domain class

---
+++ src/main/java/com/murilorb/coursespringionic/domains/Category.java
@@ -0,0 +1,53 @@
+package com.murilorb.coursespringionic.domains;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+
+	public Category() {
+	}
+
+	public Category(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
--- src/main/java/com/murilorb/coursespringionic/resources/CategoryResource.java
+++ src/main/java/com/murilorb/coursespringionic/resources/CategoryResource.java
@@ -1,16 +1,28 @@
 package com.murilorb.coursespringionic.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.murilorb.coursespringionic.domains.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
 	@GetMapping
-	public String listing() {
-		return "REST está funcionando";
+	public List<Category> listing() {
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		
+		List<Category> list = new ArrayList<>();
+		list.addAll(Arrays.asList(cat1, cat2));
+
+		return list;
 	}
 
 }
