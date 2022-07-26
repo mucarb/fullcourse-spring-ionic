aa6f6c0f5c1327e3ae3abf0d3d6808337b97914f
mucarb <murilo.rb60@gmail.com>
2022-07-26 13:43:43 -0300

    Creating instantiation operation

--- src/main/java/com/murilorb/coursespringionic/CoursespringionicApplication.java
+++ src/main/java/com/murilorb/coursespringionic/CoursespringionicApplication.java
@@ -1,13 +1,31 @@
 package com.murilorb.coursespringionic;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.murilorb.coursespringionic.domains.Category;
+import com.murilorb.coursespringionic.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CoursespringionicApplication {
+public class CoursespringionicApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursespringionicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
