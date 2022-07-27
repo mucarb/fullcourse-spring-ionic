0f0564345ce6df3d4984cdf5286b6cb5e3750553
mucarb <murilo.rb60@gmail.com>
2022-07-27 13:58:10 -0300

    Inserting a new Category with POST

--- src/main/java/com/murilorb/coursespringionic/resources/CategoryResource.java
+++ src/main/java/com/murilorb/coursespringionic/resources/CategoryResource.java
@@ -1,11 +1,16 @@
 package com.murilorb.coursespringionic.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.murilorb.coursespringionic.domains.Category;
 import com.murilorb.coursespringionic.services.CategoryService;
@@ -16,11 +21,18 @@ public class CategoryResource {
 
 	@Autowired
 	private CategoryService service;
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Category> findById(@PathVariable Integer id) {
 		Category obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Category obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/murilorb/coursespringionic/services/CategoryService.java
+++ src/main/java/com/murilorb/coursespringionic/services/CategoryService.java
@@ -21,4 +21,9 @@ public class CategoryService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Category.class.getName()));
 	}
 
+	public Category insert(Category obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
 }
