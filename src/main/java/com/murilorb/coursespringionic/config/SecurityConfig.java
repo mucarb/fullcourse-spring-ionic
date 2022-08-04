3b9c4ba86fb9596bb105ffe510d5df4f6a940d0d
mucarb <murilo.rb60@gmail.com>
2022-08-04 16:52:33 -0300

    Spring Security initial setup

---
+++ src/main/java/com/murilorb/coursespringionic/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.murilorb.coursespringionic.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+// Classe para configuracoes de seguranca
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Autowired
+	private Environment env;
+
+	// caminhos que estao liberados sem autenticacao
+	public static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
+
+	// caminhos que estao liberados sem autenticacao, acesso somente a leitura
+	public static final String[] PUBLIC_MATCHERS_GET = { "/products/**", "/categories/**" };
+
+	@Bean
+	public SecurityFilterChain basicUrlAuthenticationSettings(HttpSecurity http) throws Exception {
+		// pegando perfils ativos do projeto
+		// se caso o perfil ativo seja de teste (test), significa que quero acesso ao H2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			// camando pra liberar o acesso
+			http.headers().frameOptions().disable();
+		}
+		// desabilitando proteção a CSRF no backend
+		http.cors().and().csrf().disable();
+		// liberando autorizacao e exigindo autenticacao dos caminhos restantes
+		// PUBLIC_MATCHERS_GET caminhos somente para leitura, ou seja, requisição GET
+		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+		// garantindo que seja criado sessao de usuario
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		return http.build();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		// permitindo acesso aos endpoint por multiplas fontes com as config. basicas
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+
+}
