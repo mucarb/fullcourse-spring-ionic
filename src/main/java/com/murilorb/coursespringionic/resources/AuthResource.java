21b4d0ad4c51a487d4a23ef0e2afaf230129fbe3
mucarb <murilo.rb60@gmail.com>
2022-08-05 20:26:50 -0300

    Refresh token

---
+++ src/main/java/com/murilorb/coursespringionic/resources/AuthResource.java
@@ -0,0 +1,31 @@
+package com.murilorb.coursespringionic.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.murilorb.coursespringionic.security.JWTUtil;
+import com.murilorb.coursespringionic.security.UserSS;
+import com.murilorb.coursespringionic.services.UserService;
+
+// classe controladora responsavel por autorizacao e autenticacao
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@PostMapping(value = "/refresh_token")
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
